383e75b61b78d4bc193f3362979cf7c9aace5b79
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-26 08:56:48 +0100

    feat: created db functions

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,75 +1,87 @@
-import type { StaticDecode, TObject } from '@sinclair/typebox';
+import { type StaticDecode, type StaticEncode, Type as T } from '@sinclair/typebox';
+// import { Value as V } from '@sinclair/typebox/value';
 import { collections } from './config';
 // // import type { StaticDecode } from '@sinclair/typebox';
 // import { Effect as _, ReadonlyRecord as R, pipe, ReadonlyArray as A } from 'effect';
 
-import get_entries_loaders from '$collections';
-import { pipe, ReadonlyRecord as R, ReadonlyArray as A, Option as O } from 'effect';
+import get_entry_loaders from '$collections';
+import {
+	pipe,
+	ReadonlyRecord as R,
+	ReadonlyArray as A,
+	Option as O,
+	String as S,
+	flow
+} from 'effect';
+import { Value } from '@sinclair/typebox/value';
+import { href } from './utils';
 // import { page } from '$app/stores';
 // import { get } from 'svelte/store';
 
-export { get_entries_loaders };
+//
 
-// //
-
-export type EntryLoader = () => Promise<unknown>;
-
-export type CollectionLoader = {
-	content_schema: TObject;
-	filename_schema: TObject;
-	loader: () => Record<string, EntryLoader>;
-};
-
-// //
-
-type CollectionName = keyof typeof collections;
-type CollectionConfig<C extends CollectionName> = (typeof collections)[C];
-type CollectionType<C extends CollectionName> = StaticDecode<
-	CollectionConfig<C>['content_schema']
-> &
-	StaticDecode<CollectionConfig<C>['filename_schema']>;
+export type CollectionName = keyof typeof collections;
+export type CollectionSchema<C extends CollectionName> = (typeof collections)[C];
+export type CollectionInput<C extends CollectionName> = StaticEncode<(typeof collections)[C]>;
+export type Collection<C extends CollectionName> = StaticDecode<(typeof collections)[C]>;
 
 //
 
-export interface CollectionService<C extends CollectionName> {
-	get_one: (id: string) => Promise<unknown>;
-	current?: () => Promise<CollectionType<C>>;
+export function get_paths(): string[] {
+	return pipe(
+		get_entry_loaders(),
+		R.toEntries,
+		A.map(([entry_path]) => entry_path),
+		A.map(flow(S.replace('/+page.svelte', ''), S.replace('./', '/'), href))
+	);
 }
 
-export function collection<C extends CollectionName>(name: C): CollectionService<C> {
-	return {
-		get_one: (slug: string) =>
-			pipe(
-				get_entries_loaders(),
-				R.toEntries,
-				A.filter(([entry_name]) => entry_name.includes(`${name}/${slug}`)),
-				A.head,
-				O.map(async ([, /* entry_name */ entry_loader]) => {
-					const o = await entry_loader();
-					console.log(o);
-					return o;
-				}),
-				O.getOrThrow
-			)
-		// const e =
-		// // const o = await import(`../routes/(collections)/${name}/${slug}/+page.svelte`);
-		// console.log(o);
-		// return o;
+//
 
-		// async current() {
-		// 	const pageStore = get(page)
-		// 	pageStore.route
-		// }
-	};
+function get_collection_schema<C extends CollectionName>(collection_name: C): CollectionSchema<C> {
+	return collections[collection_name];
 }
+
+export function create<C extends CollectionName>(
+	collection_name: C,
+	data: CollectionInput<C>
+): Collection<C> {
+	const collection_schema = get_collection_schema(collection_name);
+	const parsed_value = Value.Decode(collection_schema, data);
+	return parsed_value;
+}
+
+export function get<C extends CollectionName>(
+	collection_name: C,
+	entry_name: string
+): Promise<Collection<C>> {
+	return pipe(
+		get_entry_loaders(),
+		R.toEntries,
+		A.filter(([entry_path]) => entry_path.includes(`${collection_name}/${entry_name}`)),
+		A.head,
+		O.map(([, /* entry_name */ entry_loader]) => entry_loader),
+		O.map((entry_loader) => parse_entry_loader(collection_name, entry_loader)),
+		O.getOrThrow
+	);
+}
+
+type EntryLoader = () => Promise<unknown>;
+
+// TODO - use effect
+async function parse_entry_loader<C extends CollectionName>(
+	collection_name: CollectionName,
+	entry_loader: EntryLoader
+): Promise<Collection<C>> {
+	const collection_schema = get_collection_schema(collection_name);
+	const raw_data = await entry_loader();
+	const parsed_value = Value.Decode(T.Object({ data: collection_schema }), raw_data);
+	return parsed_value.data;
+}
+
+// }
 
 // //
-
-// export function get_collection_config<C extends CollectionName>(
-// 	collectionName: C
-// ): CollectionConfig<C> {
-// 	return collections[collectionName];
-// }
 
 // export const load_collection = <C extends CollectionName>(collection_name: C) =>
 // 	_.runPromise(
