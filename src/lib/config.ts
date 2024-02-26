53082770477c7bbd67c92b19343c341786a0aefd
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-26 08:56:20 +0100

    feat: updated config structure

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,36 +1,22 @@
-import { Type as T } from '@sinclair/typebox';
+import { Type as T, type TAnySchema } from '@sinclair/typebox';
 import * as F from './fields';
-import type { CollectionLoader } from './database';
 
 //
 
 export type CollectionName = 'organizations' | 'work_experiences';
 
 export const collections = {
-	// Due sorgenti:
+	organizations: T.Object({
+		name: T.String(),
+		location: T.String()
+	}),
 
-	// 1. Il contenuto del file
-	organizations: {
-		loader: () => import.meta.glob('./organizations/*/+page.svelte'),
-		filename_schema: T.Object({}),
-		content_schema: T.Object({
-			props: T.Object({
-				name: T.String(),
-				location: T.String()
-			})
-		})
-	},
-
-	// 2. Il nome del file
-	work_experiences: {
-		loader: () => import.meta.glob('./work_experiences/*/+page.svelte'),
-		filename_schema: T.Object({
-			date_start: F.DateString('yyyy-MM'),
-			date_end: T.Union([F.DateString('yyyy-MM'), T.Literal('oggi')]),
-			organization: F.Relation('organizations')
-		}),
-		content_schema: T.Object({})
-	}
-} satisfies Record<CollectionName, CollectionLoader>;
+	work_experiences: T.Object({
+		date_start: F.DateString('yyyy-MM'),
+		// TODO - this union doesn't work with static decode!
+		date_end: T.Union([F.DateString('yyyy-MM'), T.Literal('oggi')]),
+		organization: F.Relation('organizations')
+	})
+} satisfies Record<CollectionName, TAnySchema>;
 
 // TODO: crea attraverso funzione per nascondere "satisfies" e altri tipi
