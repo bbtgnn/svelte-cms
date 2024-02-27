db35da8e0ad4c5f8bda6643e96b42c9eed2881a1
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-27 14:33:50 +0100

    feat: setup code generation

---
+++ src/lib/database_index.ts
@@ -0,0 +1,17 @@
+export default {
+  "organizations": [
+    "dyne",
+    "ff3300",
+    "la-scuola-open-source"
+  ],
+  "projects": [
+    "join",
+    "scabec",
+    "signroom"
+  ],
+  "work_experiences": [
+    "2019-07_oggi_freelance_docente",
+    "2021-05_oggi_sos",
+    "2022-10_oggi_dyne"
+  ]
+} as const
