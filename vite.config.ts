e7e7750b7c2dc4ad7c3386e4aa4b226afb10f324
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-27 14:34:13 +0100

    feat: setup code generation (#3)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import { save_database_index_plugin } from './src/lib/generation';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), save_database_index_plugin()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
