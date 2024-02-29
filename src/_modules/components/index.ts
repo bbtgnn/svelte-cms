d436af05a4b7cf7c3513f1d00665287618e13395
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-03-01 00:32:18 +0100

    refactor: rename entrycontent component

--- src/_modules/components/index.ts
+++ src/_modules/components/index.ts
@@ -1,7 +1,7 @@
 import Document from './Document.svelte';
-import EntryContent from './EntryContent.svelte';
+import DocumentContent from './DocumentContent.svelte';
 import Collection from './Collection.svelte';
 import Relation from './Relation.svelte';
 import LogBanner from './LogBanner.svelte';
 
-export { Document, EntryContent, Collection, Relation, LogBanner };
+export { Document, DocumentContent, Collection, Relation, LogBanner };
