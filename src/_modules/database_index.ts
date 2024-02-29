75c965f5ff0eeecbf56fd890b094fa26146ddf66
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-29 08:34:57 +0100

    feat: added new project

--- src/_modules/database_index.ts
+++ src/_modules/database_index.ts
@@ -2,7 +2,7 @@ import { Type as T } from '@sinclair/typebox';
 
 export const database_index_schema = T.Object({
 	organizations: T.Union([T.Literal('dyne'), T.Literal('ff3300'), T.Literal('freelance'), T.Literal('la-scuola-open-source')]),
-	projects: T.Union([T.Literal('join'), T.Literal('scabec'), T.Literal('signroom')]),
+	projects: T.Union([T.Literal('join'), T.Literal('reaction-diffusion'), T.Literal('scabec'), T.Literal('signroom')]),
 	work_experiences: T.Union([T.Literal('2019-07_oggi_freelance_docente'), T.Literal('2021-05_oggi_sos'), T.Literal('2021-09_2022-03_sos'), T.Literal('2022-10_oggi_dyne')])
 })
 
@@ -15,6 +15,7 @@ export const database_index = {
   ],
   "projects": [
     "join",
+    "reaction-diffusion",
     "scabec",
     "signroom"
   ],
