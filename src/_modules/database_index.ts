0479cda2c5b83009f0f97b0f344b0eb802b6edc4
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-03-01 12:12:50 +0100

    fix: rename file

--- src/_modules/database_index.ts
+++ src/_modules/database_index.ts
@@ -4,7 +4,7 @@ export const database_index_schema = T.Object({
 	education: T.Union([T.Literal('maturita-scientifica-sante-simone'), T.Literal('triennio-isia-urbino')]),
 	organizations: T.Union([T.Literal('axant'), T.Literal('dyne'), T.Literal('ff3300'), T.Literal('forkbomb'), T.Literal('freelance'), T.Literal('isia-urbino'), T.Literal('la-scuola-open-source'), T.Literal('liceo-scientifico-s-simone'), T.Literal('studio-ardito')]),
 	projects: T.Union([T.Literal('join'), T.Literal('reaction-diffusion'), T.Literal('scabec'), T.Literal('signroom')]),
-	work_experiences: T.Union([T.Literal('axant-ui-designer.svelte'), T.Literal('developer-dyne'), T.Literal('developer-forkbomb'), T.Literal('developer-sos'), T.Literal('ff3300-developer'), T.Literal('freelance-multi'), T.Literal('freelance-teacher'), T.Literal('sos-information-manager'), T.Literal('sos-workshop-organizer'), T.Literal('studio-ardito-designer')])
+	work_experiences: T.Union([T.Literal('axant-ui-designer'), T.Literal('developer-dyne'), T.Literal('developer-forkbomb'), T.Literal('developer-sos'), T.Literal('ff3300-developer'), T.Literal('freelance-multi'), T.Literal('freelance-teacher'), T.Literal('sos-information-manager'), T.Literal('sos-workshop-organizer'), T.Literal('studio-ardito-designer')])
 })
 
 export const database_index = {
@@ -30,7 +30,7 @@ export const database_index = {
     "signroom"
   ],
   "work_experiences": [
-    "axant-ui-designer.svelte",
+    "axant-ui-designer",
     "developer-dyne",
     "developer-forkbomb",
     "developer-sos",
