a6f223c2c0babc139b2d031c5f7e1cd243783076
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-03-07 16:47:50 +0100

    primo aggiornamento

---
+++ src/components/CardMusica.svelte
@@ -0,0 +1,55 @@
+<script>
+	export let disco;
+	export let durata;
+	export let anno;
+	export let artista;
+
+	export let image;
+</script>
+
+<div class="card_musica">
+	<img class="card_musica__immagine" src={image} alt="Copertina del disco" />
+
+	<div class="card_musica__centro">
+		<p>{artista}</p>
+		<p class="card_musica__disco">{disco}</p>
+	</div>
+
+	<div class="card_musica__footer">
+		<p>{durata}</p>
+		<p>{anno}</p>
+	</div>
+</div>
+
+<style>
+	.card_musica {
+		background-color: pink;
+		width: 300px;
+		border-radius: 20px;
+		padding: 20px;
+		color: white;
+		font-family: sans-serif;
+	}
+
+	.card_musica__disco {
+		font-size: 25px;
+	}
+
+	.card_musica__immagine {
+		object-fit: cover;
+		aspect-ratio: 1;
+		width: 100%;
+		border-radius: 10px;
+	}
+
+	.card_musica__centro {
+		margin-top: 20px;
+		margin-bottom: 20px;
+	}
+
+	.card_musica__footer {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,32 @@
-<script lang="ts">
-	import { get_collection } from '$cms';
+<script>
+	import CardMusica from '$components/CardMusica.svelte';
+
+	let disco_1 = {
+		artista: 'John cena',
+		disco: 'Ciao',
+		durata: '10:20',
+		anno: '1230',
+		image:
+			'https://images.unsplash.com/photo-1709632237343-4583d7f96f52?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8'
+	};
 </script>
 
-Home page
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@3.4.1/src/css/preflight.css" />
+</svelte:head>
+
+<CardMusica
+	artista="John cena"
+	disco="Ciao"
+	durata="10:20"
+	anno="113"
+	image="https://images.unsplash.com/photo-1709632237343-4583d7f96f52?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8"
+/>
+
+<CardMusica
+	artista="OOK"
+	disco="Altro nome"
+	durata="11:24"
+	anno="123"
+	image="https://plus.unsplash.com/premium_photo-1709311897767-f6ce7e1fc227?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8"
+/>
